60fa7f4d7d5e8034845a2273221a167241de97ae
Yuri <yuridimitre@protonmail.com>
2020-02-26 16:51:51 -0300

    add main 4

---
+++ 4/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+void menu()
+{
+    printf("\n\t\tMENU");
+    printf("\n1.)add");
+    printf("\n2.)remove");
+    printf("\n3.)list");
+    printf("\n0.)exit");
+    printf("\n>");
+}
+int main(int argc, char const *argv[])
+{
+    int vet[5], *ptr, op = 0, numb = 0, *aux;
+    ptr = vet;
+    do
+    {
+        menu();
+        scanf("%d", &op);
+        switch (op)
+        {
+        case 1:
+            if (ptr != vet + 5)
+            {
+                printf("Qual numero a ser inserido >");
+                scanf("%d", &numb);
+                *ptr = numb;
+                ptr++;
+            }
+            else
+            {
+
+                printf("Out of range");
+            }
+            break;
+        case 2:
+            if (ptr == vet + 5)
+            {
+                ptr--;
+            }
+
+            printf("Removido o %d do espaco de memoria %p", *ptr, ptr);
+            *ptr = 0;
+            if (ptr != vet)
+            {
+                ptr--;
+            }
+            break;
+        case 3:
+            aux = vet;
+            do
+            {
+                printf("%d\n", *vet);
+                aux++;
+            } while (aux != vet + 5);
+            break;
+        case 0:
+            exit(1);
+            break;
+        default:
+            break;
+        }
+    } while (1);
+
+    return 0;
+}
